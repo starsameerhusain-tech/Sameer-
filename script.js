954063a9e9c84b5c541cd47763d9fa8f37a33464
starsameerhusain-tech <star.sameerhusain@gmail.com>
2026-08-02 10:05:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 1. CONFIGURATION
 // =========================================================================
 // Replace the link below with your Google Apps Script Web App URL
-const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/16pabK6zQMYV5IYyQCegmp6iODJiD39OUvwxFNzNYRDg/edit?gid=0#gid=0";
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbxvMtq1wd_xOeAyeyNo3DYnGcxlENSgrUEfB8xkuzunz7QBqBVyhKXHRT3LuLBb6jNQTw/exec";
 
 
 // =========================================================================
